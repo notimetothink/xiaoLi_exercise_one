c279b333c99c5f5f347d96345ef76f72e5563904
lhc <375603236@qq.com>
2023-06-25 10:37:41 +0800

    【数据结构】 修改 链表

--- C/data_structure/linkLIst.cpp
+++ C/data_structure/linkLIst.cpp
@@ -8,6 +8,52 @@ typedef struct LNode
     struct LNode *next;
 }LNode,*LinkList;
 
+// 头插法创建链表
+LinkList ListHeadInsert(LinkList &L){
+    LNode *s;
+    // LinkList s;
+    ElemType x;
+    L = (LNode*)malloc(sizeof(LNode));
+    L->next = NULL;
+    scanf("%d",&x);
+    // printf("%d",x);
+    while(x!=9999){
+        s = (LinkList)malloc(sizeof(LNode));
+        s->data = x;
+        s->next = L->next;
+        L->next = s;
+        scanf("%d", &x);
+    }
+    return L;
+}
+// 尾插法创建链表
+LinkList ListTailInsert(LinkList &L){
+    LinkList s,r;
+    ElemType x;
+    L = (LinkList)malloc(sizeof(LNode));
+    r = L;
+    scanf("%d", &x);
+    while(x!=9999){
+        s = (LinkList)malloc(sizeof(LNode));
+        s->data = x;
+        r->next = s;
+        r = r->next;
+        scanf("%d", &x);
+    }
+    r->next = NULL;
+    return L;
+}
+
+// 打印链表
+void ListPrint(LinkList L){
+    L = L->next;
+    while(L){
+        printf("%3d", L->data);
+        L = L->next;
+    }
+    printf("\n");
+}
+
 // 获取链表第i元素
 bool GetElem(LinkList L,int i,ElemType &e){
     LinkList p;
@@ -27,9 +73,10 @@ bool GetElem(LinkList L,int i,ElemType &e){
 // 元素插入链表
 bool ListInsert(LinkList &L,int i,ElemType e){
     LinkList p,s;
-    p = L->next;
+    // p = L->next; //i后插入元素
+    p = L;//i前插入元素
     int j=1;
-    if(p&&j<i){
+    while(p&&j<i){
         p = p->next;
         j++;
     }
@@ -47,8 +94,8 @@ bool ListInsert(LinkList &L,int i,ElemType e){
 bool DeleteList(LinkList &L,int i,ElemType &e){
     LinkList p,q;
     int j=1;
-    p = L->next;
-    if(p&&j<i){
+    p = L;
+    while(p&&j<i){
         p = p->next;
         j++;
     }
@@ -63,6 +110,28 @@ bool DeleteList(LinkList &L,int i,ElemType &e){
 }
 
 int main(){
-    
+    LinkList L;
+    int e,i;
+    scanf("%d",&i);
+    bool a;
+    // L = ListHeadInsert(L);
+    ListTailInsert(L);
+    ListPrint(L);
+    // a = GetElem(L,3,e);
+    ListInsert(L,i,9);
+    ListPrint(L);
+    DeleteList(L,1,e);
+    ListPrint(L);
+    DeleteList(L,3,e);
+    ListPrint(L);
+    DeleteList(L,2,e);
+    ListPrint(L);
+    // a = GetElem(L,4,e);
+    // printf("%d\n", e);
+    // if(a){
+    //     printf("success\n");
+    // }else{
+    //     printf("fail\n");
+    // }
     return 0;
 }

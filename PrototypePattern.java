632ec05c88d43b92f8b366412f6ff6ee6ac546d6
lhc <375603236@qq.com>
2023-10-13 16:05:30 +0800

    【设计模式】 新增 原型和单例模式

---
+++ PrototypePattern.java
@@ -0,0 +1,37 @@
+public class PrototypePattern {
+    public static void main(String[] args) {
+        Product product1 = new Product(2023, 11.4);
+        System.out.println(product1.getId() + " " + product1.getPrice());
+        Product product2 = (Product)product1.Clone();
+        System.out.println(product2.getId() + " " + product2.getPrice() + "123");
+
+    }
+}
+
+interface Prototype{
+    public Object Clone();
+}
+
+class Product implements Prototype{
+    private int id;
+    private double price;
+    public Product() {}
+
+    public Product(int id, double price) {
+        this.id = id;
+        this.price = price;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public Object Clone(){
+        Product obj = new Product();
+        obj.id = this.id;
+        obj.price = this.price;
+        return obj;
+    }
+}
